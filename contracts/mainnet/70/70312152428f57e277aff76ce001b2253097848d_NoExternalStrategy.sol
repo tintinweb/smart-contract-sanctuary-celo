17694c50db4e4660d25923d908d173aa5defcd6f
minion <minion>
2022-08-11 21:06:48 +0000

    with 💕

---
+++ contracts/mainnet/70/70312152428f57e277aff76ce001b2253097848d_NoExternalStrategy.sol
@@ -0,0 +1,431 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.7;
+
+import "./IStrategy.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+//*********************************************************************//
+// --------------------------- custom errors ------------------------- //
+//*********************************************************************//
+error INVALID_REWARD_TOKEN();
+error TOKEN_TRANSFER_FAILURE();
+error TRANSACTIONAL_TOKEN_TRANSFER_FAILURE();
+
+/**
+  @notice
+  This strategy holds the deposited funds without transferring them to an external protocol.
+  @author Francis Odisi & Viraz Malhotra.
+*/
+contract NoExternalStrategy is Ownable, IStrategy {
+    /// @notice inbound token (deposit token) address
+    IERC20 public inboundToken;
+
+    /// @notice reward token address
+    IERC20[] public rewardTokens;
+
+    //*********************************************************************//
+    // ------------------------- external views -------------------------- //
+    //*********************************************************************//
+
+    /** 
+    @notice
+    Get strategy owner address.
+    @return Strategy owner.
+    */
+    function strategyOwner() external view override returns (address) {
+        return super.owner();
+    }
+
+    /** 
+    @notice
+    Returns the total accumulated amount (i.e., principal + interest) stored in curve.
+    Intended for usage by external clients and in case of variable deposit pools.
+    @return Total accumulated amount.
+    */
+    function getTotalAmount() external view override returns (uint256) {
+        return address(inboundToken) == address(0) ? address(this).balance : inboundToken.balanceOf(address(this));
+    }
+
+    /** 
+    @notice
+    Get the expected net deposit amount (amount minus slippage) for a given amount. Used only for AMM strategies.
+    @return net amount.
+    */
+    function getNetDepositAmount(uint256 _amount) external pure override returns (uint256) {
+        return _amount;
+    }
+
+    /** 
+    @notice
+    Returns the underlying token address.
+    @return Returns the underlying inbound (deposit) token address.
+    */
+    function getUnderlyingAsset() external view override returns (address) {
+        return address(inboundToken);
+    }
+
+    /** 
+    @notice
+    Returns the instances of the reward tokens
+    */
+    function getRewardTokens() external view override returns (IERC20[] memory) {
+        return rewardTokens;
+    }
+
+    //*********************************************************************//
+    // -------------------------- constructor ---------------------------- //
+    //*********************************************************************//
+
+    /** 
+    @param _inboundCurrency inbound currency address.
+    */
+    constructor(address _inboundCurrency, IERC20[] memory _rewardTokens) {
+        inboundToken = IERC20(_inboundCurrency);
+        for (uint256 i = 0; i < _rewardTokens.length; ) {
+            if (address(_rewardTokens[i]) == address(0)) {
+                revert INVALID_REWARD_TOKEN();
+            }
+            unchecked {
+                ++i;
+            }
+        }
+        rewardTokens = _rewardTokens;
+    }
+
+    //*********************************************************************//
+    // ------------------------- internal method -------------------------- //
+    //*********************************************************************//
+    /** 
+    @notice
+    Transfers inbound token amount back to pool.
+    @param _inboundCurrency Address of the inbound token.
+    @param _amount transfer amount
+    */
+    function _transferInboundTokenToPool(address _inboundCurrency, uint256 _amount) internal {
+        if (_inboundCurrency == address(0)) {
+            (bool success, ) = msg.sender.call{ value: _amount }("");
+            if (!success) {
+                revert TRANSACTIONAL_TOKEN_TRANSFER_FAILURE();
+            }
+        } else {
+            bool success = IERC20(_inboundCurrency).transfer(msg.sender, _amount);
+            if (!success) {
+                revert TOKEN_TRANSFER_FAILURE();
+            }
+        }
+    }
+
+    /**
+    @notice
+    Deposits funds into this contract.
+    @param _inboundCurrency Address of the inbound token.
+    @param _minAmount Used for aam strategies, since every strategy overrides from the same strategy interface hence it is defined here.
+    _minAmount isn't needed in this strategy but since all strategies override from the same interface and the amm strategies need it hence it is used here.
+    */
+    function invest(address _inboundCurrency, uint256 _minAmount) external payable override onlyOwner {}
+
+    /**
+    @notice
+    Withdraws funds from this strategy in case of an early withdrawal.
+    @param _inboundCurrency Address of the inbound token.
+    @param _amount Amount to withdraw.
+    @param _minAmount Used for aam strategies, since every strategy overrides from the same strategy interface hence it is defined here.
+    _minAmount isn't needed in this strategy but since all strategies override from the same interface and the amm strategies need it hence it is used here.
+    */
+    function earlyWithdraw(
+        address _inboundCurrency,
+        uint256 _amount,
+        uint256 _minAmount
+    ) external override onlyOwner {
+        _transferInboundTokenToPool(_inboundCurrency, _amount);
+    }
+
+    /**
+    @notice
+    Redeems funds from this strategy when the waiting round for the good ghosting pool is over.
+    @param _inboundCurrency Address of the inbound token.
+    @param _amount Amount to withdraw.
+    @param _minAmount Used for aam strategies, since every strategy overrides from the same strategy interface hence it is defined here.
+    _minAmount isn't needed in this strategy but since all strategies override from the same interface and the amm strategies need it hence it is used here.
+    @param disableRewardTokenClaim Reward claim disable flag.
+    */
+    function redeem(
+        address _inboundCurrency,
+        uint256 _amount,
+        uint256 _minAmount,
+        bool disableRewardTokenClaim
+    ) external override onlyOwner {
+        uint256 _balance = _inboundCurrency == address(0)
+            ? address(this).balance
+            : IERC20(_inboundCurrency).balanceOf(address(this));
+        // safety check since funds don't get transferred to a extrnal protocol
+        if (_amount > _balance) {
+            _amount = _balance;
+        }
+
+        _transferInboundTokenToPool(_inboundCurrency, _amount);
+
+        if (!disableRewardTokenClaim) {
+            for (uint256 i = 0; i < rewardTokens.length; ) {
+                // safety check since funds don't get transferred to a extrnal protocol
+                if (IERC20(rewardTokens[i]).balanceOf(address(this)) != 0) {
+                    bool success = IERC20(rewardTokens[i]).transfer(
+                        msg.sender,
+                        IERC20(rewardTokens[i]).balanceOf(address(this))
+                    );
+                    if (!success) {
+                        revert TOKEN_TRANSFER_FAILURE();
+                    }
+                }
+                unchecked {
+                    ++i;
+                }
+            }
+        }
+    }
+
+    /**
+    @notice
+    Returns total accumulated reward token amount.
+    @param disableRewardTokenClaim Reward claim disable flag.
+    */
+    function getAccumulatedRewardTokenAmounts(bool disableRewardTokenClaim)
+        external
+        view
+        override
+        returns (uint256[] memory)
+    {
+        uint256[] memory amounts = new uint256[](rewardTokens.length);
+        for (uint256 i = 0; i < rewardTokens.length; ) {
+            amounts[i] = rewardTokens[i].balanceOf(address(this));
+            unchecked {
+                ++i;
+            }
+        }
+        return amounts;
+    }
+}
+
+
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IStrategy {
+    function invest(address _inboundCurrency, uint256 _minAmount) external payable;
+
+    function earlyWithdraw(
+        address _inboundCurrency,
+        uint256 _amount,
+        uint256 _minAmount
+    ) external;
+
+    function redeem(
+        address _inboundCurrency,
+        uint256 _amount,
+        uint256 _minAmount,
+        bool disableRewardTokenClaim
+    ) external;
+
+    function getTotalAmount() external view returns (uint256);
+
+    function getNetDepositAmount(uint256 _amount) external view returns (uint256);
+
+    function getAccumulatedRewardTokenAmounts(bool disableRewardTokenClaim) external returns (uint256[] memory);
+
+    function getRewardTokens() external view returns (IERC20[] memory);
+
+    function getUnderlyingAsset() external view returns (address);
+
+    function strategyOwner() external view returns (address);
+}
+
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
