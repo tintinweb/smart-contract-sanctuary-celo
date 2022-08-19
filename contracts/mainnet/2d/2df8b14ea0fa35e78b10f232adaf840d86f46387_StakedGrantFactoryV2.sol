fccd6dfd90303d2877a4faf8b7ca7b3862038eab
minion <minion>
2022-08-19 05:19:26 +0000

    with 💕

---
+++ contracts/mainnet/2d/2df8b14ea0fa35e78b10f232adaf840d86f46387_StakedGrantFactoryV2.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+// https://docs.soliditylang.org/en/v0.8.10/style-guide.html
+pragma solidity ^0.8.10;
+
+contract StakedGrantFactoryV2 {
+    constructor() {}
+
+    event StakedGrantCreated(
+        address indexed owner,
+        address indexed beneficiary,
+        address qbGrantID,
+        uint96 qbApplicationID
+    );
+
+    function createStakedGrant(
+        address _beneficiary,
+        address _qbGrantID,
+        uint96 _qbApplicationID
+    ) public {
+        emit StakedGrantCreated(
+            msg.sender,
+            _beneficiary,
+            _qbGrantID,
+            _qbApplicationID
+        );
+    }
+}
