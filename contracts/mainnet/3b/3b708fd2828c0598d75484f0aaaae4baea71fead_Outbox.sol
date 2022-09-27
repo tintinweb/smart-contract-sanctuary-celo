b168b71fb83b3d8961618becf8a9ba348dd9d024
minion <minion>
2022-09-27 17:16:45 +0000

    with 💕

---
+++ contracts/mainnet/3b/3b708fd2828c0598d75484f0aaaae4baea71fead_Outbox.sol
@@ -0,0 +1,540 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/IWormhole.sol";
+import "./interfaces/TokenBridge.sol";
+import "./interfaces/Swapper.sol";
+
+///
+contract Outbox is Ownable {
+    event BridgeOut(
+        address sender,
+        bytes32 recipient,
+        uint16 destinationChain,
+        address token,
+        uint256 amount,
+        uint64 sequence
+    );
+    event InboxAdded(uint16 chainId, address inbox);
+
+    mapping(uint16 => address) inboxes;
+
+    function bridgeOut(
+        TokenBridge bridge,
+        Swapper swapper,
+        bytes32 recipient,
+        uint16 destinationChain,
+        address inputToken,
+        address tokenToBridge,
+        uint256 amount,
+        uint256 minAmountToBridge,
+        uint32 nonce,
+        address destinationBridgedToken,
+        address destinationFinalToken,
+        uint256 minFinalAmount
+    ) external payable returns (uint64 sequence) {
+        require(
+            inboxes[destinationChain] != address(0),
+            "OB: Destination chain not supported"
+        );
+        require(
+            IERC20(inputToken).transferFrom(
+                msg.sender,
+                inputToken == tokenToBridge ? address(this) : address(swapper),
+                amount
+            ),
+            "OB: Insufficient balance"
+        );
+
+        if (inputToken != tokenToBridge) {
+            swapper.swap(inputToken, tokenToBridge, minAmountToBridge);
+        }
+        uint256 amountToBridge = IERC20(tokenToBridge).balanceOf(address(this));
+        require(
+            amountToBridge >= minAmountToBridge,
+            "OB: Insufficient amount to bridge"
+        );
+
+        bytes memory payload = abi.encode(
+            recipient,
+            destinationBridgedToken,
+            destinationFinalToken,
+            minFinalAmount
+        );
+
+        IERC20(tokenToBridge).approve(address(bridge), amountToBridge);
+        sequence = bridge.transferTokensWithPayload(
+            tokenToBridge,
+            amountToBridge,
+            destinationChain,
+            bytes32(uint256(uint160(inboxes[destinationChain]))),
+            nonce,
+            payload
+        );
+
+        emit BridgeOut(
+            msg.sender,
+            recipient,
+            destinationChain,
+            tokenToBridge,
+            amountToBridge,
+            sequence
+        );
+        return sequence;
+    }
+
+    function addInbox(uint16 chainId, address inbox) external onlyOwner {
+        inboxes[chainId] = inbox;
+
+        emit InboxAdded(chainId, inbox);
+    }
+
+    /// Shouldn't be necessary, here just in case of emergency.
+    function exec(
+        address target,
+        uint256 value,
+        bytes calldata data
+    ) external onlyOwner {
+        (bool success, bytes memory reason) = target.call{value: value}(data);
+        require(success, string(reason));
+    }
+}
+
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity ^0.8.0;
+
+contract BridgeStructs {
+    struct Transfer {
+        // PayloadID uint8 = 1
+        uint8 payloadID;
+        // Amount being transferred (big-endian uint256)
+        uint256 amount;
+        // Address of the token. Left-zero-padded if shorter than 32 bytes
+        bytes32 tokenAddress;
+        // Chain ID of the token
+        uint16 tokenChain;
+        // Address of the recipient. Left-zero-padded if shorter than 32 bytes
+        bytes32 to;
+        // Chain ID of the recipient
+        uint16 toChain;
+        // Amount of tokens (big-endian uint256) that the user is willing to pay as relayer fee. Must be <= Amount.
+        uint256 fee;
+    }
+
+    struct TransferWithPayload {
+        // PayloadID uint8 = 3
+        uint8 payloadID;
+        // Amount being transferred (big-endian uint256)
+        uint256 amount;
+        // Address of the token. Left-zero-padded if shorter than 32 bytes
+        bytes32 tokenAddress;
+        // Chain ID of the token
+        uint16 tokenChain;
+        // Address of the recipient. Left-zero-padded if shorter than 32 bytes
+        bytes32 to;
+        // Chain ID of the recipient
+        uint16 toChain;
+        // Address of the message sender. Left-zero-padded if shorter than 32 bytes
+        bytes32 fromAddress;
+        // An arbitrary payload
+        bytes payload;
+    }
+
+    struct TransferResult {
+        // Chain ID of the token
+        uint16 tokenChain;
+        // Address of the token. Left-zero-padded if shorter than 32 bytes
+        bytes32 tokenAddress;
+        // Amount being transferred (big-endian uint256)
+        uint256 normalizedAmount;
+        // Amount of tokens (big-endian uint256) that the user is willing to pay as relayer fee. Must be <= Amount.
+        uint256 normalizedArbiterFee;
+        // Portion of msg.value to be paid as the core bridge fee
+        uint256 wormholeFee;
+    }
+
+    struct AssetMeta {
+        // PayloadID uint8 = 2
+        uint8 payloadID;
+        // Address of the token. Left-zero-padded if shorter than 32 bytes
+        bytes32 tokenAddress;
+        // Chain ID of the token
+        uint16 tokenChain;
+        // Number of decimals of the token (big-endian uint256)
+        uint8 decimals;
+        // Symbol of the token (UTF-8)
+        bytes32 symbol;
+        // Name of the token (UTF-8)
+        bytes32 name;
+    }
+
+    struct RegisterChain {
+        // Governance Header
+        // module: "TokenBridge" left-padded
+        bytes32 module;
+        // governance action: 1
+        uint8 action;
+        // governance paket chain id: this or 0
+        uint16 chainId;
+        // Chain ID
+        uint16 emitterChainID;
+        // Emitter address. Left-zero-padded if shorter than 32 bytes
+        bytes32 emitterAddress;
+    }
+
+    struct UpgradeContract {
+        // Governance Header
+        // module: "TokenBridge" left-padded
+        bytes32 module;
+        // governance action: 2
+        uint8 action;
+        // governance paket chain id
+        uint16 chainId;
+        // Address of the new contract
+        bytes32 newContract;
+    }
+}
+
+
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity ^0.8.0;
+
+import "./Structs.sol";
+
+interface IWormhole is Structs {
+    event LogMessagePublished(
+        address indexed sender,
+        uint64 sequence,
+        uint32 nonce,
+        bytes payload,
+        uint8 consistencyLevel
+    );
+
+    function publishMessage(
+        uint32 nonce,
+        bytes memory payload,
+        uint8 consistencyLevel
+    ) external payable returns (uint64 sequence);
+
+    function parseAndVerifyVM(bytes calldata encodedVM)
+        external
+        view
+        returns (
+            Structs.VM memory vm,
+            bool valid,
+            string memory reason
+        );
+
+    function verifyVM(Structs.VM memory vm)
+        external
+        view
+        returns (bool valid, string memory reason);
+
+    function verifySignatures(
+        bytes32 hash,
+        Structs.Signature[] memory signatures,
+        Structs.GuardianSet memory guardianSet
+    ) external pure returns (bool valid, string memory reason);
+
+    function parseVM(bytes memory encodedVM)
+        external
+        pure
+        returns (Structs.VM memory vm);
+
+    function getGuardianSet(uint32 index)
+        external
+        view
+        returns (Structs.GuardianSet memory);
+
+    function getCurrentGuardianSetIndex() external view returns (uint32);
+
+    function getGuardianSetExpiry() external view returns (uint32);
+
+    function governanceActionIsConsumed(bytes32 hash)
+        external
+        view
+        returns (bool);
+
+    function isInitialized(address impl) external view returns (bool);
+
+    function chainId() external view returns (uint16);
+
+    function governanceChainId() external view returns (uint16);
+
+    function governanceContract() external view returns (bytes32);
+
+    function messageFee() external view returns (uint256);
+}
+
+
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity ^0.8.0;
+
+interface Structs {
+    struct Provider {
+        uint16 chainId;
+        uint16 governanceChainId;
+        bytes32 governanceContract;
+    }
+
+    struct GuardianSet {
+        address[] keys;
+        uint32 expirationTime;
+    }
+
+    struct Signature {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        uint8 guardianIndex;
+    }
+
+    struct VM {
+        uint8 version;
+        uint32 timestamp;
+        uint32 nonce;
+        uint16 emitterChainId;
+        bytes32 emitterAddress;
+        uint64 sequence;
+        uint8 consistencyLevel;
+        bytes payload;
+        uint32 guardianSetIndex;
+        Signature[] signatures;
+        bytes32 hash;
+    }
+}
+
+
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity ^0.8.0;
+
+interface Swapper {
+    function swap(
+        address from,
+        address to,
+        uint256 requiredAmount
+    ) external payable;
+}
+
+
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity ^0.8.0;
+
+import "./BridgeStructs.sol";
+
+interface TokenBridge {
+    function transferTokensWithPayload(
+        address token,
+        uint256 amount,
+        uint16 recipientChain,
+        bytes32 recipient,
+        uint32 nonce,
+        bytes memory payload
+    ) external payable returns (uint64 sequence);
+
+    function completeTransferWithPayload(bytes memory encodedVm)
+        external
+        returns (bytes memory);
+
+    function parseTransferWithPayload(bytes memory encoded)
+        external
+        pure
+        returns (BridgeStructs.TransferWithPayload memory transfer);
+}
